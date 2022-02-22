f4e9b329c5f42c947bb4df63cf24eced0efc01c1
NL-ysy <yung9579@naver.com>
2022-02-22 08:26:54 +0000

    TodoApp edit ok

--- src/TodoApp.js
+++ src/TodoApp.js
@@ -1,11 +1,46 @@
-import React from 'react'
+import React, { useState, useCallback, useRef } from 'react'
 import TodoList from './TodoList';
 import TodoForm from './TodoForm';
 
 const TodoApp = () => {
+    const[todos, setTodos] = useState([
+        {
+            id: 1,
+            text: 'TDD 배우기',
+            done: true,
+        },
+        {
+            id: 2,
+            text: 'react-testing-library 사용하기',
+            done: true,
+        },
+    ]);
+    const nextId = useRef(3); //새로 추가 할 항목에서 사용할 id
+    const onInsert = useCallback(
+        text => {
+            setTodos(
+                todos.concat(
+                {
+                    id : nextId.current, 
+                    text, 
+                    done: false
+                }
+                ));
+             nextId.current += 1;
+            },
+            [todos]
+    );
+    const onToggle =useCallback(
+        id => { 
+            setTodos(
+            // js 에서는 같다를 === 세개로 함(메모리에 있는 값을 비교하고싶다)
+            todos.map(todo => todo.id === id ? {...todo, done: !todo.done}: todo)
+            );
+        },[todos]
+    );
     return (<div>
-        <TodoForm data-testid='helloworld' />
-        <TodoList todos={[]}/>
+        <TodoForm data-testid='helloworld' onInsert={onInsert}/>
+        <TodoList todos={todos} onToggle={onToggle}/>
     </div>);
 };
 export default TodoApp;
--- src/TodoApp.test.js
+++ src/TodoApp.test.js
@@ -1,11 +1,33 @@
-import React from 'react'
-import { render } from '@testing-library/react'
-import TodoApp from './TodoApp'
+import React from 'react';
+import { fireEvent, render } from '@testing-library/react';
+import TodoApp from './TodoApp';
 
 describe('<TodoApp/>', ()=> {
-    it('renders TodoFrom and TodoList', ()=>{
-        const {getByText, getByTestId} = render(<TodoApp/>);
+    it('renders TodoFrom and TodoList', () => {
+        const { getByText, getByTestId } = render(<TodoApp/>);
         getByText('등록');
         getByTestId('TodoList');
+    });
+    it('renders two defaults todos', () => {
+        const { getByText }= render(<TodoApp />);
+        getByText('TDD 배우기');
+        getByText('react-testing-library 사용하기');
+    });
+    it('creates new todo', () => {
+        const {getByPlaceholderText, getByText } = render(<TodoApp />);
+        fireEvent.change(getByPlaceholderText('할 일을 입력해주세요~'), 
+        {target: {value: '새 항목 추가 하기'},
+        });
+        fireEvent.click(getByText('등록'));
+        getByText('새 항목 추가 하기');
+    });
+    it('toggles todo', () => {
+        const {getByText} = render(<TodoApp />);
+        const todoText = getByText('TDD 배우기');
+        expect(todoText).toHaveStyle('text-descoration: line-through;');
+        fireEvent.click(todoText);
+        expect(todoText).not.toHaveStyle('text-descoration: line-through;');
+        fireEvent.click(todoText);
+        expect(todoText).toHaveStyle('text-descoration: line-through;');
     });
 });
