5087e31adfcac0e1ca30e85129ca4709ab1fc3fe
NL-ysy <yung9579@naver.com>
2022-02-22 02:47:06 +0000

    TodoForm Submit event test ok

--- src/TodoForm.test.js
+++ src/TodoForm.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import{render} from '@testing-library/react';
+import{fireEvent, render} from '@testing-library/react';
 import TodoForm from './TodoForm';
 
 describe('<TodoForm />', () => {
@@ -7,5 +7,21 @@ describe('<TodoForm />', () => {
         const{getByText, getByPlaceholderText} = render(<TodoForm/>);
         getByPlaceholderText('할 일을 입력하세요~'); //input 있는지 없는지
         getByText('등록'); //button이 있는지 없는지
+    })
+    it('change input', ()=> {
+        const{getByPlaceholderText} = render(<TodoForm/>);
+        const input = getByPlaceholderText('할 일을 입력하세요~'); //input 있는지 없는지
+        fireEvent.change(input, {target: {value: 'TDD 배우기'}});
+        expect(input).toHaveAttribute('value', 'TDD 배우기');
+    })
+    it ('call onInsert and clears input',() => {
+        const onInsert =jest.fn();
+        const{getByText, getByPlaceholderText} =render(<TodoForm onInsert={onInsert} />);
+        const input = getByPlaceholderText('할 일을 입력하세요~');
+        const button = getByText('등록');
+        fireEvent.change(input, {target: {value: 'TDD 배우기'}});
+        fireEvent.click(button);
+        expect(onInsert).toBeCalledWith('TDD 배우기');
+        expect(input).toHaveAttribute('value', '');
     })
 })
