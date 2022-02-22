31753b917cb39654ad95edb603b03f7d6dfbbf80
NL-ysy <yung9579@naver.com>
2022-02-22 03:28:38 +0000

    TodoForm refactoring ok

--- src/TodoForm.test.js
+++ src/TodoForm.test.js
@@ -7,5 +7,6 @@ describe('<TodoForm />', () => {
         const{getByText, getByPlaceholderText} = render(<TodoForm/>);
         getByPlaceholderText('할 일을 입력하세요~'); //input 있는지 없는지
         getByText('등록'); //button이 있는지 없는지
+        
     })
 })
